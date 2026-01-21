39299495d81d9178fa6e55d7943ce10125aa47eb
FrodoFox <f.foxwell1@gmail.com>
2026-01-21 12:24:14 +0000

    Automated update before transfer

--- MotorControl.py
+++ MotorControl.py
@@ -1,43 +1,31 @@
-import smbus
 import time
 
-# Constants for I2C Motor Driver V1.3
-I2C_ADDR = 0x10  # Default I2C address for Motor Driver V1.3
-STEPPER_MOTOR = 0x01  # Example motor ID (check your hardware docs)
-CMD_SET_SPEED = 0x82
-CMD_STEP = 0x1A
+from grove.grove_i2c_motor_driver import MotorDriver
 
-class StepperMotorDriver:
-    def __init__(self, bus_num=1, address=I2C_ADDR):
-        self.bus = smbus.SMBus(bus_num)
-        self.address = address
+motor = MotorDriver()
 
-    def set_speed(self, speed):
-        """
-        Set the speed of the stepper motor.
-        :param speed: Speed value (0-255)
-        """
-        speed = max(0, min(255, speed))
-        self.bus.write_i2c_block_data(self.address, CMD_SET_SPEED, [STEPPER_MOTOR, speed])
+while True:
 
-    def step(self, steps, direction=1):
-        """
-        Move the stepper motor a number of steps.
-        :param steps: Number of steps to move
-        :param direction: 1 for forward, 0 for backward
-        """
-        steps = max(0, min(255, steps))
-        self.bus.write_i2c_block_data(self.address, CMD_STEP, [STEPPER_MOTOR, direction, steps])
+    # speed range: 0(lowest) - 100(fastest)
 
-    def stop(self):
-        """
-        Stop the stepper motor.
-        """
-        self.set_speed(0)
-
-if __name__ == "__main__":
-    motor = StepperMotorDriver()
     motor.set_speed(100)
-    motor.step(50, direction=1)
-    time.sleep(1)
-    motor.stop()
+
+    # channel 1 only
+    
+    # to set channel 1&2: motor.set_speed(100, 100)
+    
+    # direction: True(clockwise), False(anti-clockwise)
+    
+    motor.set_dir(True)
+    
+    # channel 1 only,
+    
+    # to set channel 1&2: motor.set_dir(True, True)
+    
+    time.sleep(2)
+    
+    motor.set_speed(70)
+    
+    motor.set_dir(False)
+    
+    time.sleep(2)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SDP-Group-20
+
+Rather than clone the git repo (because the SDPRobots wifi doesn't let you do that). Instead I've written an ssh script that will copy the local files to the bash script (the entire git repo) and clone that onto the pi (to simulate a git repo). To put the updated files onto the pi and github at the same time, all you need to do is run: <br>
+```./ssh_run.expect``` <br>
+from the local directory inside the git repo
+
+Documentation for Motor and Wiring <br>
+Motor Board:
+```https://uoe.sharepoint.com/:w:/r/sites/InformaticsMakerspace/Shared%20Documents/Guides/Raspberry%20Pi%20Guides/Raspberry%20Pi%20Guide%202025.docx?d=w2834da8f1fa643468df0b08edc16ed89&csf=1&web=1&e=1g7MJi```
