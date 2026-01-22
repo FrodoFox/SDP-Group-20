c6c9d29af4b2850c12f65d002dc82b4a6e57b092
FrodoFox <f.foxwell1@gmail.com>
2026-01-22 12:25:09 +0000

    Automated update before transfer

--- MotorControl.py
+++ MotorControl.py
@@ -3,14 +3,11 @@ from grove.grove_i2c_motor_driver import MotorDriver
 
 motor = MotorDriver()
 
-<<<<<<< HEAD
 motor.set_dir(True)
 motor.set_speed(255)
 
 print("Motor spinning continuously. Ctrl+C to stop.")
 
-=======
->>>>>>> 69c4fa7129d95c2d746c9acc2b57d97a54254af6
 try:
     while True:
         motor.StepperRun(200, 50)  
